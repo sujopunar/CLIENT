f43fea7ac71d300064d864e102ffc9cd6b890de0
naveedalirehmani <naveedalirehmani321@gmail.com>
2022-03-19 19:43:09 +0500

    added search bar for admin

--- client/adminDashboard/app.js
+++ client/adminDashboard/app.js
@@ -1,56 +1,118 @@
 
-
+// THIS WILL HADNLE LOGOUT 
 const btn = document.querySelector('.logout')
 
-btn.onclick = ()=>{
-    localStorage.setItem('adminLogedIn',"false")
-    window.location.href = '/client/index.html'
+btn.onclick = () => {
+  localStorage.setItem('adminLogedIn', "false")
+  window.location.href = '/client/index.html'
 }
 
-//ALL USERS ARE HERE
 
-//FETCHING ALL USERS
-async function getAllUsers (){
-    
+//THIS FUNCTION WILL FETCH ALL THE USERS AND RENDER THEM..
+async function renderAllUsers(param, searchData) {
+
+  let userData; // THIS IS AN ARRAY CONTAINING USERS OBJECTS THAT HAS TO BE RENDERED ON SCREEN.
+
+  if (param) {
+    // HERE WE ARE FETHING ALL USERS.
     const response = await fetch('http://localhost:3000/allusers')
-    const userData= await response.json()
+    userData = await response.json()
+  } else {
+    // HERE ARE ONLY FETCHING SEARCH RESULT USERS.
+    userData = searchData.allSearchUsers
+  }
 
-    const tbody = document.querySelector('tbody')
-    console.log(userData)
-    console.log(tbody)
 
-    
-    
-    const td = document.createElement('td')
-    for(let k = 0; k<userData.length;k++){
-        console.log(userData[k].lastBillPaidDate)
-        const duration = window.moment().diff(window.moment(userData[k].lastBillPaidDate),'months');
-        let image = false
-        if(userData[k].lastBillPaidImage == 'noImage'){
-            image = `<a>No Bill Paid</a>`
-        }else{
-            image = `<a href=${userData[k].lastBillPaidImage} download=${'billImageOf'+userData[k].username+".png"}>BILL IMAGE</a>`
-        }
-        
-            const tr = document.createElement("tr")
-            // console.log(userData[k].lastBillPaidDate)
-            tr.innerHTML = `
-                <th scope="row">${k}</th>
-                <td>${userData[k].username}</td>
-                <td>${userData[k].lastBillPaidDate}</td>
-                <td>${duration} MONTHS
-                </td>
-                <td>${image}</td>
+
+  const tbody = document.querySelector('tbody')
+  // SETTING INNER HTML = "" SO THAT WE CAN START NEW EACH TIME THIS FUNCITON IS CALLED.
+  tbody.innerHTML = ""
+
+  // THIS WILL RENDER USERS WITH JAVASCRIPT DOM.
+  for (let k = 0; k < userData.length; k++) {
+
+    // FINDING USER LAST PAID DATE.
+    const duration = window.moment().diff(window.moment(userData[k].lastBillPaidDate), 'months');
+
+    // CONDITIONAL RENDERING OF IMG TAG DEPENDING ON IF THE USER HAS SUBMIT A RECEIPT
+    let image = "";
+    if (userData[k].lastBillPaidImage == 'noImage') {
+      image = `<a>No Bill Paid</a>`
+    } else {
+      image = `<a href=${userData[k].lastBillPaidImage} download=${'billImageOf' + userData[k].username + ".png"}>BILL IMAGE</a>`
+    }
+    let src=userData[k].profile==="noImage"?"./profile.png":userData[k].profile
+
+    // JAVASCIRPT DOM TO RENDER BOOTSTRAP TABLE
+    const tr = document.createElement("tr")
+    tr.innerHTML = `
+            <td >
+              <div class="d-flex align-items-center">
+              <img
+              src=${src}
+              alt=""
+              style="width: 45px; height: 45px"
+              class="rounded-circle"
+              />
+                <div class="ms-3">
+                  <p class="fw-bold mb-1">${userData[k].firstName} ${userData[k].lastName}</p>
+                  <p class="text-muted mb-0">${userData[k].email}</p>
+                </div>
+              </div>
+            </td>
+            <td>
+              <p class="text-muted mb-0">${userData[k].phone}</p>
+              </td>
+              <td>${duration} Months</td>
+              <td>${userData[k].street}</td>
+              <td>
+                  <span style="background:${duration > 0 ? "red" : "green"};border-radius:10px;font-size:0.8rem;padding:0.2rem;color:white">${duration > 0 ? "NOT PAID" : "PAID"}</span>
+              </td>
+              <td>
+                  ${image}
+              </td>
+            </td>
             `
-            tbody.append(tr)
-        }
 
-    console.log('here')
-//     
+    // APPENDING TR INTO TBODY
+    tbody.append(tr)
+  }
+
+
+  const searchBtn = document.querySelector('.searchBtn');
+  const searchInput = document.querySelector('.searchBar')
+
+  // THIS FUNCTION WILL BE CALLED WHEN YOU SEARCH FOR SPECIFIC USERS.
+  searchBtn.onclick = async () => {
+
+    //FETCHING SPECIFIC SEARCH USERS.
+    const response = await fetch('http://localhost:3000/users', {
+      method: "POST",
+      body: JSON.stringify({
+        firstName: searchInput.value
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+
+    const data = await response.json()
+
+    // CALLING THE RENDER FUNCTION AGAIN BUT THIS TIME RENDERING ON SPECIFIC SEARCH USERS.
+    renderAllUsers(false, data)
+
+    searchInput.addEventListener('input', function () {
+      // THIS WILL CALL RENDER FUNCTION AGAIN WHEN INPUT IS CLEARED OUT.
+      if (!this.value) renderAllUsers(true)
+    });
+
+  }
 
 }
 
-getAllUsers();
+
+// THIS IS THE MAIN FUNCTION THAT WILL RENDER USERS.
+renderAllUsers(true);
 
 
 
--- server/routes/allUsers.js
+++ server/routes/allUsers.js
@@ -1,12 +1,11 @@
 const {Router} = require('express');
 const router = new Router();
 const {signup} = require('../models/signup')
-const bcrypt = require('bcrypt')
 
 router.get('/allusers',async (request,response)=>{
     const allUsers = await signup.find().where({
         isAdmin: false
-    }).select("username lastBillPaidDate lastBillPaidImage ")
+    }).select("firstName lastName email phone street _Id lastBillPaidImage profile lastBillPaidDate")
     response.json(allUsers)    
 })
 
@@ -16,20 +15,11 @@ router.get('/user/:id',async (request,response)=>{
     response.json(user)    
 })
 
-// router.get('/admin',async(request,response)=>{
+router.post('/users',async(request,response)=>{
+    const { firstName } = request.body;
+    const allSearchUsers = await signup.find({$or:[{firstName:{$regex: firstName}},{lastName:{$regex: firstName}},{email:{$regex: firstName}}]});
+    response.json({allSearchUsers})
+})
 
-//     const salt = await bcrypt.genSalt(10);
-//     const hashedPassword = await bcrypt.hash("ADMIN", salt);
-
-//     const responseData = new signup({
-//         username:'ADMIN',
-//         password: hashedPassword,
-//         isAdmin:true,
-//         phoneNumber:1234
-//     })
-//     responseData.save()
-
-//     response.json('done')
-// })
 
 module.exports = router;
